e7867bf60c7fb0ecc42dc729b5726f87b1cbd0d6
kevit-kshitij-antani <kshitij.antani@kevit.io>
2022-09-05 17:12:37 +0530

    Adds feature of CRUD routes for user

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+    console.log("Create user account");
+    res.send('Hello GET');
+})
+
+app.get('/', function (req, res) {
+    console.log("Read user account");
+    res.send('Hello GET');
+})
+
+app.get('/', function (req, res) {
+    console.log("Delete user account");
+    res.send('Hello GET');
+})
+
+app.get('/', function (req, res) {
+    console.log("Update user account");
+    res.send('Hello GET');
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
