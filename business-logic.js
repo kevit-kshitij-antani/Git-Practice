bb5a84a7664376d03125a063bdd9b353e9722ebd
kevit-kshitij-antani <kshitij.antani@kevit.io>
2022-09-06 10:03:43 +0530

    A file was deleted

--- business-logic.js
+++
@@ -1,6 +0,0 @@
-app.get('/', function (req, res) {
-    console.log("Got a GET request for the homepage");
-    const userCount = Math.ceil(Math.random() * 100);
-    res.send(`Hello GET. Users are: ${userCount}`);
- })
- 
